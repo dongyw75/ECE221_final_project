2e3e5cea0b2fe83a3cd67434f7ae78b2f7db7d97
Lisa Cohen <ljcohen@ucdavis.edu>
2016-11-30 10:52:39 -0800

    updates

--- DESeq/DESeq_flies.R
+++ DESeq/DESeq_flies.R
@@ -1,4 +1,5 @@
 #http://bioconductor.org/packages/release/bioc/vignettes/DESeq/inst/doc/DESeq.R
+
 library(DESeq)
 library("lattice")
 library(biomaRt)
@@ -6,8 +7,7 @@ library(tximport)
 library(readr)
 library("RColorBrewer")
 setwd("~/Documents/UCDavis/ECE221/ECE221_final_project")
-source('~/Documents/scripts/plotPCAWithSampleNames.R')
-flymatrix<-read.csv("flies_space_counts_salmon.csv")
+#flymatrix<-read.csv("flies_space_counts_salmon.csv")
 # SRR3390478.quant = G1R1
 # SRR3390479.quant = G1R2
 # SRR3390480.quant = G1R3
@@ -16,21 +16,7 @@ flymatrix<-read.csv("flies_space_counts_salmon.csv")
 # SRR3390483.quant = G3R2
 # SRR3390484.quant = G3R3 = G1R1
 # SRR3390485.quant = G3R4
-colnames(flymatrix)<-c("Name","G3R3","G1R2","G1R3","G1R4","G3R1","G3R2","G1R1","G3R4")
-#colnames(flymatrix)<-c("Name","G1R1","G1R2","G1R3","G1R4","G3R1","G3R2","G3R3","G3R4")
-head(flymatrix)
-rownames(flymatrix)<-flymatrix$Name
-flymatrix<-flymatrix[,c(2:9)]
-colnames(flymatrix)
-head(flymatrix)
-# get annotations
-ensembl=useMart("ensembl")
-ensembl_id<-rownames(flymatrix)
-ensembl = useDataset("dmelanogaster_gene_ensembl",mart=ensembl)
-query<-getBM(attributes=c('flybase_transcript_id','ensembl_gene_id','external_gene_name','description'), filters = 'flybase_transcript_id', values = ensembl_id, mart=ensembl)
-tx2gene<-query[,c(1,2)]
-colnames(tx2gene)<-c("TXNAME","GENEID")
-####
+
 # import salmon
 dir<-"/Users/cohenl06/Documents/UCDavis/ECE221/ECE221_final_project"
 list.files("~/Documents/UCDavis/ECE221/ECE221_final_project")
@@ -54,10 +40,10 @@ cds = estimateSizeFactors( cds )
 notAllZero = (rowSums(counts(cds))>0)
 cdsFullBlind = estimateDispersions( cds, method = "blind" )
 vsdFull = varianceStabilizingTransformation( cdsFullBlind )
-plotPCAWithSampleNames(vsdFull, intgroup="condition", ntop=40000)
 plotPCA(vsdFull, intgroup="condition")
 
 sizeFactors( cds )
+
 head( counts( cds, normalized=TRUE ) )
 cds = estimateDispersions( cds )
 str( fitInfo(cds) )
@@ -70,50 +56,71 @@ stopifnot(identical(colnames(res), c("id", "baseMean", "baseMeanA", "baseMeanB",
                                      "log2FoldChange", "pval", "padj")))
 plotMA(res)
 hist(res$pval, breaks=100, col="skyblue", border="slateblue", main="")
-resSig = res[ res$padj < 0.05, ]
 
 
-write.csv( res, file="DESeq/hypergravity_space_flies_DESeq_all.csv" )
-write.csv(resSig,file="DESeq/hypergravity_space_flies_DESeq_padj0.05.csv")
-
-
-dds$condition <- relevel(dds$condition, "G1")
-dds<-DESeq(dds,betaPrior=FALSE)
-norm_counts<-counts(dds,normalized=TRUE)
-norm_counts_data<-as.data.frame(norm_counts)
-dim(norm_counts_data)
-filtered_norm_counts<-norm_counts_data[!rowSums(norm_counts_data==0)>=1, ]
-dim(filtered_norm_counts)
-head(filtered_norm_counts)
+# get counts
+counts_table = counts( cds, normalized=TRUE )
+filtered_norm_counts<-counts_table[!rowSums(counts_table==0)>=1, ]
 GeneID<-rownames(filtered_norm_counts)
 filtered_norm_counts<-cbind(filtered_norm_counts,GeneID)
 dim(filtered_norm_counts)
+# get annotations
+ensembl=useMart("ensembl")
+ensembl_id<-rownames(flymatrix)
+ensembl = useDataset("dmelanogaster_gene_ensembl",mart=ensembl)
+query<-getBM(attributes=c('flybase_transcript_id','ensembl_gene_id','external_gene_name','description'), filters = 'flybase_transcript_id', values = ensembl_id, mart=ensembl)
+#
 colnames(query)
 col.names<-c("flybase_transcript_id","GeneID","external_gene_id","description")
 colnames(query)<-col.names
 query_gene<-query[,c(2,3,4)]
 dim(query_gene)
 merge_biomart_res_counts <- merge(filtered_norm_counts,query_gene,by=unique("GeneID"))
-counts<-merge_biomart_res_counts[!duplicated(merge_biomart_res_counts$GeneID), ]
-head(counts)
-dim(counts)
-###
-plotDispEsts(dds)
-log_dds<-rlog(dds)
-plotPCAWithSampleNames(log_dds, intgroup="condition", ntop=40000)
-###
-res<-results(dds,contrast=c("condition","G3","G1"))
-res_ordered<-res[order(res$padj),]
-GeneID<-rownames(res_ordered)
-res_ordered<-as.data.frame(res_ordered)
-res_ordered<-cbind(res_ordered,GeneID)
-merge_biomart_res_counts <- merge(counts,res_ordered,by="GeneID")
-merge_biomart_res_all<-subset(merge_biomart_res_counts,merge_biomart_res_counts$padj!="NA")
-merge_biomart_res_all<-merge_biomart_res_all[order(merge_biomart_res_all$padj),]
-dim(merge_biomart_res_all)
-###
-res_merged_cutoff<-subset(merge_biomart_res_all,merge_biomart_res_all$padj<0.05)
-dim(res_merged_cutoff)
-plot(log2(res$baseMean), res$log2FoldChange, col=ifelse(res$padj < 0.05, "red","gray67"),main="(DESeq2) G3 vs. G1 (padj<0.05, ±log2FC = 1)",xlim=c(1,20),pch=20,cex=1,ylim=c(-12,12))
-plot(log2(res$baseMean),res$log2FoldChange[res$padj, < 0.05,])
+merge_biomart_res_counts<-merge_biomart_res_counts[!duplicated(merge_biomart_res_counts[,c(1)]),]
+dim(merge_biomart_res_counts)
+head(merge_biomart_res_counts)
+head(res)
+colnames(res)
+colnames(res)<-c("GeneID","baseMean","baseMeanA","baseMeanB","foldChange","log2FoldChange","pval","padj")
+merged<-merge(merge_biomart_res_counts,res,by="GeneID")
+merged<-merged[order(merged[,18]), ]
+write.csv(merged, file="DESeq/hypergravity_space_flies_DESeq_all.csv" )
+resSig = merged[ merged$padj < 0.05, ]
+#write.csv(resSig,file="DESeq/hypergravity_space_flies_DESeq_padj0.05.csv")
+#
+plot(log2(merged$baseMean), merged$log2FoldChange, col=ifelse(merged$padj < 0.05, "red","gray67"),main="(DESeq2) G3 vs. G1 (padj<0.05, log2FC = ±1)",xlim=c(1,20),pch=20,cex=1,ylim=c(-12,12))
 abline(h=c(-1,1), col="blue")
+# heatmap
+up_down<-resSig
+dim(up_down)
+up_down_1FC<-subset(up_down,up_down$log2FoldChange>1 | up_down$log2FoldChange< -1)
+write.csv(up_down_1FC,file="DESeq/hypergravity_space_flies_DESeq_padj0.05.csv")
+dim(up_down_1FC)
+d<-up_down_1FC
+d<-na.omit(d)
+dim(d)
+d<-up_down_1FC[,c(2:9)]
+d<-as.matrix(d)
+d<-as.data.frame(d)
+d<-as.matrix(d)
+rownames(d) <- up_down_1FC[,10]
+d<-type.convert(d)
+head(d)
+colnames(d)
+d<-d[,c(7,2,3,4,5,6,1,8)]
+colnames(d)
+
+hr <- hclust(as.dist(1-cor(t(d), method="pearson")), method="complete")
+mycl <- cutree(hr, h=max(hr$height/1.5))
+clusterCols <- rainbow(length(unique(mycl)))
+myClusterSideBar <- clusterCols[mycl]
+myheatcol <- greenred(75)
+heatmap.2(d, main="G3 vs. G1 (padj<0.05, log2FC = ±1)", 
+          Rowv=as.dendrogram(hr),
+          cexRow=0.75,cexCol=0.8,srtCol= 90,
+          adjCol = c(NA,0),offsetCol=2.5, 
+          Colv=NA, dendrogram="row", 
+          scale="row", col=myheatcol, 
+          density.info="none", 
+          trace="none", RowSideColors= myClusterSideBar)
+
--- DESeq2/DESeq2_flies.R
+++ DESeq2/DESeq2_flies.R
@@ -1,3 +1,4 @@
+
 library(DESeq2)
 library("lattice")
 library(biomaRt)
